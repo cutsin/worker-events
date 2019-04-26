263c90d62d4b5c084197722845c7433808403ec6
bbc <bbc>
2019-04-26 15:39:28 +0800

    Update babel

--- cjs/index.js
+++ cjs/index.js
@@ -1,10 +1,11 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.default = void 0;
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -16,46 +17,62 @@ var WorkerEvents = function WorkerEvents() {
 
   _classCallCheck(this, WorkerEvents);
 
-  this.inWindow = (typeof window === 'undefined' ? 'undefined' : _typeof(window)) === 'object';
-  this.listeners = {} /* ...Map() */
+  this.inWindow = (typeof window === "undefined" ? "undefined" : _typeof(window)) === 'object';
+  this.listeners = {}
+  /* ...Map() */
   // emit
-  ;this.emit = function (eventname, data) {
+  ;
+
+  this.emit = function (eventname, data) {
     eventId++;
-    var msg = { data: data, eventId: eventId
-      // Always trigger event to self-env asynchronous
-    };setTimeout(function () {
+    var msg = {
+      data: data,
+      eventId: eventId // Always trigger event to self-env asynchronous
+
+    };
+    setTimeout(function () {
       var listener = _this.listeners[eventname];
+
       if (listener) {
         listener.forEach(function (opts, cb) {
           cb(msg);
           if (opts && opts.once) _this.off(eventname, cb);
         });
       }
-    });
-    // In worker, also post to window
+    }); // In worker, also post to window
+
     if (!_this.inWindow) {
-      postMessage(Object.assign(msg, { eventname: eventname, cmd: 'worker-events' }));
+      postMessage(Object.assign(msg, {
+        eventname: eventname,
+        cmd: 'worker-events'
+      }));
     }
-  };
-  //
+  }; //
+
+
   this.off = function (eventname, cb) {
     var listener = _this.listeners[eventname];
     if (listener && listener.has(cb)) listener.delete(cb);
-  };
-  // on
+  }; // on
+
+
   this.on = function (eventname, cb, opts) {
     var listener = _this.listeners[eventname];
+
     if (listener) {
       listener.set(cb, opts);
     } else {
       _this.listeners[eventname] = new Map([[cb, opts]]);
     }
   };
+
   this.once = function (eventname, cb) {
-    _this.on(eventname, cb, { once: true });
+    _this.on(eventname, cb, {
+      once: true
+    });
   };
 };
 
 var workerEvents = new WorkerEvents();
-
-exports.default = workerEvents;
+var _default = workerEvents;
+exports.default = _default;
--- mjs/index.js
+++ mjs/index.js
@@ -4,46 +4,62 @@ let eventId = 1;
 class WorkerEvents {
   constructor() {
     this.inWindow = typeof window === 'object';
-    this.listeners = {} /* ...Map() */
+    this.listeners = {}
+    /* ...Map() */
     // emit
-    ;this.emit = (eventname, data) => {
+    ;
+
+    this.emit = (eventname, data) => {
       eventId++;
-      const msg = { data, eventId
-        // Always trigger event to self-env asynchronous
-      };setTimeout(() => {
+      const msg = {
+        data,
+        eventId // Always trigger event to self-env asynchronous
+
+      };
+      setTimeout(() => {
         const listener = this.listeners[eventname];
+
         if (listener) {
           listener.forEach((opts, cb) => {
             cb(msg);
             if (opts && opts.once) this.off(eventname, cb);
           });
         }
-      });
-      // In worker, also post to window
+      }); // In worker, also post to window
+
       if (!this.inWindow) {
-        postMessage(Object.assign(msg, { eventname, cmd: 'worker-events' }));
+        postMessage(Object.assign(msg, {
+          eventname,
+          cmd: 'worker-events'
+        }));
       }
-    };
-    //
+    }; //
+
+
     this.off = (eventname, cb) => {
       const listener = this.listeners[eventname];
       if (listener && listener.has(cb)) listener.delete(cb);
-    };
-    // on
+    }; // on
+
+
     this.on = (eventname, cb, opts) => {
       const listener = this.listeners[eventname];
+
       if (listener) {
         listener.set(cb, opts);
       } else {
         this.listeners[eventname] = new Map([[cb, opts]]);
       }
     };
+
     this.once = (eventname, cb) => {
-      this.on(eventname, cb, { once: true });
+      this.on(eventname, cb, {
+        once: true
+      });
     };
   }
+
 }
 
 const workerEvents = new WorkerEvents();
-
 export default workerEvents;
